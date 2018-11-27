d7ed09666514f99281ee320fd76ca405012f140b
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2018-11-26 21:52:13 -0800

    Final database demonstration

--- MySQLQueries.sql
+++ MySQLQueries.sql
@@ -8,10 +8,10 @@ SELECT is_explicit FROM song WHERE song_name = 'Odyssey';
 SELECT publish_date from album WHERE album_name = 'Odyssey';
 
 /*What band wrote Resonance?*/
-SELECT band_name FROM band, band_Song, song WHERE song.song_name = 'Resonance' AND song.id = band_song.song_id;
+SELECT band_name FROM band, band_song, song WHERE song.song_name = 'Resonance' AND song.id = band_song.song_id AND band.id = band_song.band_id;
 
-/*What band wrote the album Odyssey*/
-SELECT band_name FROM band, album_band, album WHERE album.album_name = 'Odyssey' AND album.id = album_band.album_id;
+/*What band wrote the album Crack The Skye*/
+SELECT band_name FROM band, album_band, album WHERE album.album_name = 'Crack The Skye' AND album.id = album_band.album_id AND band.id = album_band.band_id;
 
 /*How many songs did the band home write?*/
 SELECT count(band_id) FROM band_song, band where band.band_name = 'Home' AND band_song.band_id = band.id;
